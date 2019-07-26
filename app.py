7367a77e5375ecb871baff347b4edd64bf0ad30a
Ubuntu <ubuntu@ip-172-31-36-107.ec2.internal>
2019-07-26 21:49:36 +0000

    DB connection added

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'tea'
+app.config["MONGO_URI"] = 'mongodb+srv://cat:catUser@myfirstcluster-lypt3.mongodb.net/tea?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_category')
+def get_category():
+    return render_template("category.html", category=mongo.db.category.find())
 
 
 if __name__ == '__main__':
