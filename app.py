ae8c7cbd360105d9f31a1dedc887c91bed4f0e1d
Ubuntu <ubuntu@ip-172-31-36-107.ec2.internal>
2019-08-09 21:58:33 +0000

    login and register features added

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
+import bcrypt
 
 app = Flask(__name__)
 
@@ -9,12 +10,54 @@ app.config["MONGO_DBNAME"] = 'tea'
 app.config["MONGO_URI"] = 'mongodb+srv://cat:catUser@myfirstcluster-lypt3.mongodb.net/tea?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
-
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 @app.route('/get_category')
 def get_category():
     return render_template("home.html", category=mongo.db.category.find())
+    
+    
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({'username' : request.form['username']})
+
+        if existing_user is None:
+            pw_hash = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+    
+            db_password = pw_hash.decode("utf-8")
+           
+            users.insert({'username' : request.form['username'], 'password' : db_password})
+          
+            session['username'] = request.form['username']
+            return redirect(url_for('login'))
+        
+    return render_template('register.html')
+    
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+
+    if request.method == 'POST':
+        users = mongo.db.users
+        login_user = users.find_one({'username' : request.form['username']})
+    
+        if login_user:
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+                session['username'] = request.form['username']
+                return redirect(url_for('get_category'))
+    
+        
+    return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for('get_category'))    
+
    
 @app.route('/get_tea/<category_name>', methods=['GET', 'POST'])
 def get_tea(category_name):
